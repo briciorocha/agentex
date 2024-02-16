25ad8b0c71d53041212cb8a3524a74fc95bf5a01
briciorocha <jbricio.nrocha@gmail.com>
2024-02-15 21:54:54 -0300

    Initial commit

---
+++ src/main/java/br/com/njbr/agentex/AgentexApplication.java
@@ -0,0 +1,13 @@
+package br.com.njbr.agentex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/njbr/agentex/AgentexApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.njbr.agentex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
